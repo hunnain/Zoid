26bb7b661ae317b26f917e0f65c23ed27dc92a52
Hunnain Pasha <hunnainpashahgchgc@gmail.com>
2019-01-15 18:38:37 +0500

    Zoid

---
+++ login.js
@@ -0,0 +1,11 @@
+
+window.MyLoginZoidComponent = zoid.create({
+
+    // The html tag used to render my component
+
+    tag: 'my-login-component',
+
+    // The url that will be loaded in the iframe or popup, when someone includes my component on their page
+
+    url: './login.html'
+});
